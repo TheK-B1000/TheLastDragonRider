d1efc49a3dfe4ea5888807695cb0b6def0395242
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2021-12-07 14:23:53 -0500

    08 - Match Animation To Movement

--- Assets/Game/Mover.cs
+++ Assets/Game/Mover.cs
@@ -23,7 +23,7 @@ public class Mover : MonoBehaviour
         {
             MoveToCursor();
         }
-
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -35,6 +35,14 @@ public class Mover : MonoBehaviour
         {
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 
 }
