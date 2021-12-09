ecc4a6a8cfc52729fb1b3fc82752d81e4b06b39c
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2021-12-09 15:40:19 -0500

    09 - Basic Movement Tweaks

--- Assets/Game/FollowCamera.cs
+++ Assets/Game/FollowCamera.cs
@@ -7,14 +7,8 @@ public class FollowCamera : MonoBehaviour
 
     [SerializeField] Transform target;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = target.position;
     }
